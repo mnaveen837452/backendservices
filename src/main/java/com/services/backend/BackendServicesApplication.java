be54dadb2c408ff3f4178e98b4da9df7716e917c
Naveen <naveenkumarvrp@gmai.com>
2022-04-14 15:19:43 +0530

    Added project files into project directory

---
+++ src/main/java/com/services/backend/BackendServicesApplication.java
@@ -0,0 +1,13 @@
+package com.services.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendServicesApplication.class, args);
+	}
+
+}
